47bf808127262b30532db7c64f84a763c9f273ac
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2020-12-07 01:28:48 +0100

    0.2.0 - Greater window handling, instance storage, log options, callbacks
    - Switch between fullscreen & windowed
    - VSYNC can be enabled
    - Callbacks are editable
    - Instance storage
    - Render function which polls events and updates FPS

--- inc/SSS/GLFW/Window.hpp
+++ inc/SSS/GLFW/Window.hpp
@@ -5,21 +5,95 @@
 
 __SSS_GLFW_BEGIN
 
+    // --- Callbacks ---
+
+__INTERNAL_BEGIN
+// Resizes the internal width and height of correspondig Window instance
+void window_resize_callback(GLFWwindow* ptr, int w, int h);
+__INTERNAL_END
+
+    // --- Window class ---
+    
 class Window {
+    
+    friend void _internal::window_resize_callback(GLFWwindow* ptr, int w, int h);
+
 public:
+// --- Log options ---
+    
+    struct LOG {
+        static bool constructor;
+        static bool destructor;
+        static bool fps;
+    };
+
+// --- Public aliases ---
+
+    using Shared = std::shared_ptr<Window>;
+
+private:
+// --- Instances storage (private) ---
+
+    using Weak = std::weak_ptr<Window>;
+
+    // Instances of created windows
+    static std::map<GLFWwindow const*, Weak> _instances;
+
     // Constructor, creates a window and makes its context current
-    Window(int w, int h, std::string const& title = "Untitled");
+    // Private, to be called via Window::create();
+    Window(int w, int h, std::string const& title);
+
+public :
+// --- Instances storage (public) ---
+
     // Destructor
     ~Window();
+
+    // Creates a window and returns a corresponding shared_ptr
+    static Shared create(int w, int h, std::string const& title = "Untitled");
+    // To be used in callbacks.
+    // Returns an existing Window instance, via its GLFWwindow pointer
+    static Shared get(GLFWwindow const* ptr);
+
+// --- Public methods ---
 
     // Wether the user requested to close the window.
     // NOTE: this simply is a call to glfwWindowShouldClose
     bool shouldClose() const noexcept;
 
+    // Enables or disables the VSYNC of the window
+    void setVSYNC(bool state);
+    // Enables or disables fullscreen mode on given screen
+    void setFullscreen(bool state, int screen_id = 0);
+
+    // Renders a frame & polls events.
+    // Logs fps if specified in LOG structure.
+    void render();
+
+    // Sets a corresponding callback
+    template<typename _Func>
+    void setCallback(_Func(*set)(GLFWwindow*, _Func), _Func callback) {
+        set(_window.get(), callback);
+    };
+
 private:
-    int _w;
-    int _h;
+// --- Private variables ---
+
+    // Window size
+    int _w; // Width
+    int _h; // Height
+
+    // Windowed to Fullscreen variables
+    int _windowed_x{ 0 };   // Old x (left) pos
+    int _windowed_y{ 0 };   // Old y (up) pos
+    int _windowed_w{ 0 };   // Old width
+    int _windowed_h{ 0 };   // Old height
+    
+    // Window ptr. Automatically destroyed
     _internal::GLFWwindow_Ptr _window;
+
+    // FPS Timer
+    FPS_Timer fps_timer;
 };
 
 __SSS_GLFW_END
--- src/Window.cpp
+++ src/Window.cpp
@@ -2,38 +2,180 @@
 
 __SSS_GLFW_BEGIN
 
+    // --- Static initializations ---
+
+// Default log options
+bool Window::LOG::constructor{ true };
+bool Window::LOG::destructor{ true };
+bool Window::LOG::fps{ true };
+
+// Static members
+std::map<GLFWwindow const*, Window::Weak> Window::_instances{};
+
+    // --- Callbacks ---
+
+__INTERNAL_BEGIN
+
+// Resizes the internal width and height of correspondig Window instance
+void window_resize_callback(GLFWwindow* ptr, int w, int h)
+{
+    Window::Shared window = Window::get(ptr);
+    window->_w = w;
+    window->_h = h;
+}
+
+__INTERNAL_END
+
+    // --- Constructor & destructor ---
+
 // Constructor, creates a window and makes its context current
 Window::Window(int w, int h, std::string const& title) try
     : _w(w), _h(h)
 {
+    // Hints
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+
     // Create window
     _window.reset(glfwCreateWindow(w, h, title.c_str(), nullptr, nullptr));
     // Throw if an error occured
-    if (!_window) {
+    if (!_window.get()) {
         const char* msg;
         glfwGetError(&msg);
         throw_exc(msg);
     }
-
-    // Initialize GLEW
-    // TODO : understand wtf this does
+    // Make its context current
     glfwMakeContextCurrent(_window.get());
 
-    __LOG_CONSTRUCTOR
+    // Initialize GLEW
+    GLenum const glew_ret = glewInit();
+    // Throw if an error occured
+    if (glew_ret != GLEW_OK) {
+        char const* msg = reinterpret_cast<char const*>(glewGetErrorString(glew_ret));
+        throw_exc(msg);
+    }
+
+    // Set window callbacks
+    setCallback(glfwSetWindowSizeCallback, _internal::window_resize_callback);
+    // Set VSYNC to false by default
+    setVSYNC(false);
+
+    if (LOG::constructor) {
+        __LOG_CONSTRUCTOR
+    }
 }
 __CATCH_AND_RETHROW_METHOD_EXC
 
 // Destructor
 Window::~Window()
 {
-    __LOG_DESTRUCTOR
+    if (LOG::destructor) {
+        __LOG_DESTRUCTOR
+    }
 }
+
+    // --- Instances storage ---
+
+// Creates a window and returns a corresponding shared_ptr
+Window::Shared Window::create(int w, int h, std::string const& title) try
+{
+    Shared window = Shared(new Window(w, h, title));
+    _instances.emplace(window->_window.get(), window);
+    return window;
+}
+__CATCH_AND_RETHROW_FUNC_EXC
+
+// Returns an existing Window instance by its GLFWwindow pointer
+Window::Shared Window::get(GLFWwindow const* ptr) try
+{
+    return _instances.at(ptr).lock();
+}
+__CATCH_AND_RETHROW_FUNC_EXC
+
+    // --- Public methods ---
 
 // Wether the user requested to close the window.
 // NOTE: this simply is a call to glfwWindowShouldClose
 bool Window::shouldClose() const noexcept
 {
     return glfwWindowShouldClose(_window.get());
+}
+
+// Enables or disables the VSYNC of the window
+void Window::setVSYNC(bool state)
+{
+    // Ensure the current context is this window
+    GLFWwindow* ptr = glfwGetCurrentContext();
+    bool const isCurrent = ptr == _window.get();
+    // Swap context if needed
+    if (!isCurrent) {
+        glfwMakeContextCurrent(_window.get());
+    }
+    // Set VSYNC
+    glfwSwapInterval(state);
+    // Re-swap context if needed
+    if (!isCurrent) {
+        glfwMakeContextCurrent(ptr);
+    }
+}
+
+// Enables or disables fullscreen mode on given screen
+void Window::setFullscreen(bool state, int screen_id)
+{
+    int monitor_count;
+    GLFWmonitor** monitors = glfwGetMonitors(&monitor_count);
+    GLFWmonitor* currentMonitor = glfwGetWindowMonitor(_window.get());
+    
+    if (state) {
+        // Ensure given ID is in range
+        if (screen_id >= monitor_count) {
+            __LOG_METHOD_WRN("screen_id out of range.");
+            return;
+        }
+        // Ensure window isn't already fullscreen on given ID
+        if (currentMonitor == monitors[screen_id]) {
+            __LOG_METHOD_WRN("window is already fullscreen on given screen");
+            return;
+        }
+        // Store current size & pos, if currently in windowed mode
+        if (currentMonitor == nullptr) {
+            _windowed_w = _w;
+            _windowed_h = _h;
+            glfwGetWindowPos(_window.get(), &_windowed_x, &_windowed_y);
+        }
+        // Get monitor's size
+        GLFWvidmode const* mode = glfwGetVideoMode(monitors[screen_id]);
+        // Set window in fullscreen
+        glfwSetWindowMonitor(_window.get(), monitors[screen_id],
+            0, 0, mode->width, mode->height, GLFW_DONT_CARE);
+    }
+    else {
+        // Ensure the window isn't arealdy windowed
+        if (currentMonitor == nullptr) {
+            __LOG_METHOD_WRN("window is already windowed.");
+            return;
+        }
+        // Set window in windowed mode with old values
+        glfwSetWindowMonitor(_window.get(), nullptr,
+            _windowed_x, _windowed_y, _windowed_w, _windowed_h, GLFW_DONT_CARE);
+    }
+}
+
+// Renders a frame & polls events.
+// Logs fps if specified in LOG structure.
+void Window::render()
+{
+    // Render back buffer
+    glfwSwapBuffers(_window.get());
+    // Update fps, log if needed
+    if (fps_timer.addFrame()) {
+        if (LOG::fps) {
+            __LOG_MSG(toString(fps_timer.get()) + "fps");
+        }
+    }
+    // Clear back buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Poll events
+    glfwPollEvents();
 }
 
 __SSS_GLFW_END
