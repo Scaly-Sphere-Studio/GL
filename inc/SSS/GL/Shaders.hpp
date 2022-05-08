2e6d2831e411ba1af769ce807defdf19da9139d7
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2022-05-08 12:16:15 +0200

    Document Shaders

--- inc/SSS/GL/Shaders.hpp
+++ inc/SSS/GL/Shaders.hpp
@@ -17,35 +17,61 @@ namespace SSS::Log::GL {
 
 SSS_GL_BEGIN;
 
+/** Non-exhaustive abstractization of \b OpenGL shaders.
+ *  @sa Window::createShaders(), Renderer::setShadersID()
+ */
 class Shaders : public _internal::WindowObject {
     friend class Window;
     friend class Renderer;
 
 private:
-    // Constructor : loads shaders and links them to a program
+    // Constructor 
     Shaders(std::weak_ptr<Window> window);
 
 public:
+    /** Destructor, unloads internal glProgram if needed.
+     *  @sa Window::removeShaders(), Window::cleanObjects().
+     */
     ~Shaders();
     
-    // Aliases
+    /** Unique ptr stored in Window objects.*/
     using Ptr = std::unique_ptr<Shaders>;
+    /** Internal preset shaders IDs*/
     enum class Preset : uint32_t {
-        Plane = 0x80000000,
-
-        First = Plane
+        /** Indicates that further ID values are reserved.*/
+        First = 0x80000000,
+        /** Plane shaders, used by Plane::Renderer by default.*/
+        Plane
     };
 
+    /** Loads shaders from raw strings (useful for Preset shaders).
+     *  Context will always be accurately set.
+     *  @sa loadFromFiles()
+     */
     void loadFromStrings(std::string const& vertex_data, std::string const& fragment_data);
+    /** Loads shaders from filepaths.
+     *  Context will always be accurately set.
+     *  @sa loadFromStrings()
+     */
     void loadFromFiles(std::string const& vertex_fp, std::string const& fragment_fp);
 
-    // Use this shader program for the current rendering
+    /** Simple handle to \c glUseProgram().
+     *  Context will always be accurately set.
+     */
     void use() const;
 
-    // Return the location of a uniform variable for this program
+    /** Simple handle to \c glGetUniformLocation().
+     *  Context will always be accurately set.
+     */
     GLint getUniformLocation(std::string const& name);
 
+    /** Simple handle to \c glUniform1iv().
+     *  Context will always be accurately set.
+     */
     void setUniform1iv(std::string const& name, GLsizei count, const GLint* value);
+    /** Simple handle to \c glUniformMatrix4fv().
+     *  Context will always be accurately set.
+     */
     void setUniformMat4fv(std::string const& name, GLsizei count,
         GLboolean transpose, GLfloat const* value);
 
