b343c6d17a9d228504747a53ebefd5e81af9300f
Mollier Yoann <yoann.mollier@gmail.com>
2026-03-29 04:07:10 +0200

    Log, Export Material, Render States

--- src/Objects/Materials.cpp
+++ src/Objects/Materials.cpp
@@ -4,7 +4,65 @@
 
 SSS_GL_BEGIN;
 
+void RenderState::update()
+{
+    // Apply RenderState
+    if (blend)
+    {
+        glEnable(GL_BLEND);
+        glBlendFunc(blendSrc, blendDst);
+    }
+    else glDisable(GL_BLEND);
+
+    if (depthclear) glClear(GL_DEPTH_BUFFER_BIT);
+    if (depthTest)
+    {
+        glDepthMask(depthWrite);
+        glDepthFunc(depthFunc);
+    }
+    else glDisable(GL_DEPTH_TEST);
+
+    //glCullFace(cullFace);
+}
 
 
+
+Material::Material(Shaders::Shared shader):_shader(shader) {}
+
+void Material::set(const std::string& name, UniformValue val)
+{
+    _uniforms[name] = val;
+}
+
+void Material::bind()
+{
+
+    if (!_shader) {
+        LOG_METHOD_WRN("No shaders bound");
+        return;
+    }
+
+    _shader->use();
+    state.update();
+
+
+    // Push uniforms
+    int texSlot = 0;
+    for (auto& [name, val] : _uniforms) {
+        _shader->setUniform(name, val);
+    }
+}
+
+void Material::unbind() const
+{
+    glActiveTexture(GL_TEXTURE0);
+    glUseProgram(0);
+}
+
+void Material::watch() const
+{
+	_shader->watch();
+}
 
 SSS_GL_END;
+
